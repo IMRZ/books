57b181c7c280f5753e6814ff5efec090fd365270
Imad Razzak <im.razzak@gmail.com>
2020-02-18 09:58:52 +0100

    cf188a1

---
+++ precache-manifest.68f1ee26dc7f6f2bba1232a35eb84a3d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffef6c195caccfd2ef41",
+    "url": "/books/css/app.38ed4bab.css"
+  },
+  {
+    "revision": "3d39c077d9dcbb4d3ce3fa5759c3682f",
+    "url": "/books/img/avoid_using_dark_patterns.3d39c077.jpg"
+  },
+  {
+    "revision": "a70d80d07e5fc2a6fe41f36b10663391",
+    "url": "/books/img/car1.a70d80d0.jpg"
+  },
+  {
+    "revision": "4344ec705b949170ea82d0e321cfe35b",
+    "url": "/books/img/car2.4344ec70.jpg"
+  },
+  {
+    "revision": "56ae25dda3dc014935546613c0de19a3",
+    "url": "/books/img/car3.56ae25dd.jpg"
+  },
+  {
+    "revision": "a97e4c968d1f32ea50982c33b1855fef",
+    "url": "/books/img/car4.a97e4c96.jpg"
+  },
+  {
+    "revision": "5efc10086802ea76145917d0496e4cc7",
+    "url": "/books/img/copy_pasta_stackoverflow.5efc1008.jpg"
+  },
+  {
+    "revision": "d1a008f397146d05674dd0989b8c303d",
+    "url": "/books/img/hoping_this_works.d1a008f3.jpg"
+  },
+  {
+    "revision": "eb6738a40a22c8a4ee4a8c5ef0b7b678",
+    "url": "/books/img/pointless_meetings.eb6738a4.jpg"
+  },
+  {
+    "revision": "caf2e7e5f6828df1344dcd08cc469e46",
+    "url": "/books/img/trying_stuff.caf2e7e5.jpg"
+  },
+  {
+    "revision": "6bf5934e14b0aa17b1d5b616748af320",
+    "url": "/books/index.html"
+  },
+  {
+    "revision": "ffef6c195caccfd2ef41",
+    "url": "/books/js/app.700405c3.js"
+  },
+  {
+    "revision": "76f7cf76b7e7499118dd",
+    "url": "/books/js/chunk-vendors.36371846.js"
+  },
+  {
+    "revision": "2a8aa7b08861f47ed5c3ece6d1dd398c",
+    "url": "/books/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/books/robots.txt"
+  }
+]);
